006e8e114b7559efa110ada0becb6bcaf8237c4f
kyle-judd <juddkyle1408@gmail.com>
2020-09-08 16:33:21 -0400

    added a basic toolbar

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,28 +1,28 @@
-import React from 'react';
-import classes from './Burger.module.css';
-import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+import React from "react";
+import classes from "./Burger.module.css";
+import BurgerIngredient from "./BurgerIngredient/BurgerIngredient";
 
-const burger = props => {
-    let transformedIngredients = Object.keys(props.ingredients)
-        .map(igKey => {
-            return [...Array(props.ingredients[igKey])].map((_, index) => {
-                return <BurgerIngredient key={igKey + index} type={igKey} />
-            });
-        })
-        .reduce((arr, el) => {
-            return arr.concat(el);
-        }, []);
-    if (transformedIngredients.length === 0) {
-        transformedIngredients = <p>Please start adding ingredients!</p>
-    }
-    console.log(transformedIngredients);
-    return (
-        <div className={classes.Burger}>
-            <BurgerIngredient type="top-bun" />
-            {transformedIngredients}
-            <BurgerIngredient type="bottom-bun" />
-        </div>
-    );
+const burger = (props) => {
+  let transformedIngredients = Object.keys(props.ingredients)
+    .map((igKey) => {
+      return [...Array(props.ingredients[igKey])].map((_, index) => {
+        return <BurgerIngredient key={igKey + index} type={igKey} />;
+      });
+    })
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
+  console.log(transformedIngredients);
+  return (
+    <div className={classes.Burger}>
+      <BurgerIngredient type="top-bun" />
+      {transformedIngredients}
+      <BurgerIngredient type="bottom-bun" />
+    </div>
+  );
 };
 
 export default burger;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import Aux from '../../hoc/Aux';
-import classes from './Layout.module.css';
+import React from "react";
+import Aux from "../../hoc/Aux";
+import classes from "./Layout.module.css";
+import Toolbar from "../Navigation/Toolbar/Toolbar";
 
 const Layout = (props) => (
-    <Aux>
-        <div>Toolbar, SideDrawer, Backdrop</div>
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
+  <Aux>
+    <Toolbar />
+    <main className={classes.Content}>{props.children}</main>
+  </Aux>
 );
 
 export default Layout;
