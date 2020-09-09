f8d57dac18b38ffe67b4097f206740b717a7360e
kyle-judd <juddkyle1408@gmail.com>
2020-09-09 12:16:37 -0400

    added a toggle for the sidebar

--- src/components/Navigation/Sidedrawer/Sidedrawer.js
+++ src/components/Navigation/Sidedrawer/Sidedrawer.js
@@ -3,17 +3,26 @@ import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import NavigationItem from "../NavigationItems/NavigationItem/NavigationItem";
 import classes from "./Sidedrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
 const Sidedrawer = (props) => {
+  let attachedClasses = [classes.Sidedrawer, classes.Closed];
+  if (props.open) {
+    attachedClasses = [classes.Sidedrawer, classes.Open];
+  }
   return (
-    <div className={classes.Sidedrawer}>
-      <div className={classes.Logo}>
-        <Logo />
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedClasses.join(" ")}>
+        <div className={classes.Logo}>
+          <Logo />
+        </div>
+        <nav>
+          <NavigationItems />
+        </nav>
       </div>
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+    </Aux>
   );
 };
 
