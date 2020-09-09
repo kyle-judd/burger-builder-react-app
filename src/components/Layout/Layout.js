e7e8ddb8e7ec4dcb80bb3d095153241f6ba639c2
kyle-judd <juddkyle1408@gmail.com>
2020-09-08 20:28:35 -0400

    added sidedrawer (hamburger)

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import Sidedrawer from "../Navigation/Sidedrawer/Sidedrawer";
 
 const Layout = (props) => (
   <Aux>
     <Toolbar />
+    <Sidedrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
